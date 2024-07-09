8e517c2ce7a85bad8fdb19f7dd5c966c4f0b0120
b0xercat <seojin3154@naver.com>
2024-07-09 17:06:17 +0900

    feat: edit root layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>shadcn button</Button>
-    </main>
-  );
+  return <h1>Main</h1>;
 }
