02a62ffd981ccc50744fa2adc442fa5eafda8221
jieun <suspen064@gmail.com>
2024-07-11 22:42:37 +0900

    feat: add prettier setting, adjust prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,18 +5,18 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Right Paper",
-  description: "Youtube Fake News Detect Service",
+	title: "Right Paper",
+	description: "Youtube Fake News Detect Service",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
