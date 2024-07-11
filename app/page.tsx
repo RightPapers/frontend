717060941cbcb10018706ba51e59265433bdcf24
jieun <suspen064@gmail.com>
2024-07-12 00:00:00 +0900

    feat: adjust prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Button>shadcn button</Button>
     </main>
   );
