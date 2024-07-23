3281316d890433ac1c6012140ba7a64404dd58b5
b0xercat <seojin3154@naver.com>
2024-07-23 13:52:11 +0900

    feat: implement history card component

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -1,3 +1,26 @@
+import HistoryCard from '@/components/HistoryCard';
+
+const dummyHistory = [
+  {
+    video_title: '긴급속보 일본 화산 대폭발!당장 일본을 도망쳐야 한다!',
+    channel_title: '페페TV',
+    thumbnails: 'https://img.youtube.com/vi/FRqTqguKe6c/sddefault.jpg',
+  },
+  {
+    video_title: '안냥하세요!!!',
+    channel_title: '스토리 온',
+    thumbnails: 'https://img.youtube.com/vi/u069h5Dm_JI/sddefault.jpg',
+  },
+];
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className='flex w-[385px] mobile:w-full items-center bg-white'>
+      <div className='flex w-full justify-around'>
+        {dummyHistory.slice(0, 2).map((history, index) => {
+          return <HistoryCard key={index} {...history} />;
+        })}
+      </div>
+    </div>
+  );
 }
---
+++ components/HistoryCard.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+export default function HistoryCard({
+  video_title,
+  channel_title,
+  thumbnails,
+}: {
+  video_title: string;
+  channel_title: string;
+  thumbnails: string;
+}) {
+  return (
+    <div className='flex w-36 flex-col gap-4'>
+      <Image
+        src={thumbnails}
+        alt={video_title}
+        width={144}
+        height={80}
+        className='h-20 w-36 rounded-lg object-cover'
+      />
+      <div className='flex flex-col gap-1'>
+        <p className='line-clamp-2 text-xs font-semibold'>{video_title}</p>
+        <p className='line-clamp-1 text-[11px] text-gray-500'>{channel_title}</p>
+      </div>
+    </div>
+  );
+}
