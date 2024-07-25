ccab7c92aac95fdb4290d4f7c36fc79de9418f49
jieun <suspen064@gmail.com>
2024-07-25 21:18:40 +0900

    style: fix bg color

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className='flex min-h-screen w-full items-center justify-center bg-gray-400 px-5'>
+    <main className='flex min-h-screen w-full items-center justify-center bg-gray-200 px-5'>
       <div className='w-full max-w-[384px] mobile:max-w-full'>{children}</div>
     </main>
   );
