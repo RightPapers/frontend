808b182240ec58c63ba2e18d4707e327ff748603
b0xercat <seojin3154@naver.com>
2024-08-01 15:21:12 +0900

    feat: implement post for analyze and feedback

--- app/api/feedback/route.ts
+++ app/api/feedback/route.ts
@@ -3,3 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   return NextResponse.json({ message: 'GET feedback' });
 }
+
+export async function POST(request: Request) {
+  return NextResponse.json({ message: '피드백 감사합니다!' });
+}
