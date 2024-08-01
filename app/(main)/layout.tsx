5ab74ba209b0180c4d3221cdae98c88eacf42077
b0xercat <seojin3154@naver.com>
2024-08-01 23:06:08 +0900

    chore: execute prettier write

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className='flex min-h-screen w-full items-center justify-center bg-gray-200 px-5'>
-      <div className='max-w-mobile w-full mobile:max-w-full'>{children}</div>
+      <div className='w-full max-w-mobile mobile:max-w-full'>{children}</div>
     </main>
   );
 }
