f13608bfd2e6dcc7c0a3e2477de4722d18a00f92
b0xercat <seojin3154@naver.com>
2024-08-09 23:23:00 +0900

    feat: add zod and react-hook-form to validate youtube url

--- app/(main)/(home)/_components/LinkComponent.tsx
+++ app/(main)/(home)/_components/LinkComponent.tsx
@@ -1,8 +1,20 @@
 import { Button } from '@/components/ui/button';
-import InputComponent from './InputComponent';
 import CardComponent from '@/components/CardComponent';
 import LinkHeader from './LinkHeader';
 import { LoadingState } from '@/lib/types';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormField } from '@/components/ui/form';
+import InputComponent from './InputComponent';
+
+const youtubeUrlPattern = /^(https?:\/\/)?(www\.)?(youtu\.be|youtube\.com)/;
+
+const formSchema = z.object({
+  url: z.string().regex(youtubeUrlPattern, {
+    message: '유효한 유튜브 링크를 입력해주세요.',
+  }),
+});
 
 // TODO: POST 요청하는 함수로 수정
 const fetchData = async () => {
@@ -15,23 +27,47 @@ const LinkComponent = ({
 }: {
   setLoadingState: (state: LoadingState) => void;
 }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { url: '' },
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+    // setLoadingState(LoadingState.start);
+    await fetchData();
+    // setLoadingState(LoadingState.done);
+  };
+
   return (
     <CardComponent>
       <LinkHeader />
-      <form className='flex flex-col gap-8'>
-        <InputComponent />
-        <Button
-          variant='main'
-          onClick={async (e) => {
-            e.preventDefault();
-            setLoadingState(LoadingState.start);
-            await fetchData();
-            setLoadingState(LoadingState.done);
-          }}
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='flex flex-col gap-8'
         >
-          검색
-        </Button>
-      </form>
+          <FormField
+            control={form.control}
+            name='url'
+            render={({ field }) => (
+              <FormControl>
+                <div className='flex flex-col gap-1'>
+                  <InputComponent field={field} />
+                  {form.formState.errors.url && (
+                    <p className='text-sm text-red-500'>
+                      {form.formState.errors.url.message}
+                    </p>
+                  )}
+                </div>
+              </FormControl>
+            )}
+          />
+          <Button variant='main' type='submit'>
+            검색
+          </Button>
+        </form>
+      </Form>
     </CardComponent>
   );
 };
