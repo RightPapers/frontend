1dc3673beb5e6fd131f3d8fb87b117f7c2cb226e
b0xercat <seojin3154@naver.com>
2024-08-10 01:17:51 +0900

    feat: edit result page to get data from local storage

--- app/(main)/result/page.tsx
+++ app/(main)/result/page.tsx
@@ -1,14 +1,35 @@
+'use client';
+
 import AccuracyDashboard from './_components/AccuracyDashboard';
 import FeedbackCard from './_components/FeedbackCard';
 import SummaryCard from './_components/SummaryCard';
 import ArticleCards from './_components/ArticleCards';
+import { useResultStore } from '@/lib/store';
+import { useSearchParams } from 'next/navigation';
 
 const Result = () => {
+  const searchParams = useSearchParams();
+
+  const id = searchParams.get('id');
+  const results = useResultStore((state) => state.results);
+
+  const result = id
+    ? results.find((result) => result.id === id)
+    : results[results.length - 1] || null;
+
+  // TODO: 잘못된 ID로 접근하는 경우의 UI 추가
+  if (!result) {
+    return null;
+  }
+
+  const { analysis_result, related_articles } = result.result;
+  const accuracy = 100 - analysis_result.fake_possibility;
+
   return (
     <>
-      <AccuracyDashboard accuracy={30} />
-      <SummaryCard />
-      <ArticleCards />
+      <AccuracyDashboard accuracy={accuracy} />
+      <SummaryCard accuracy={accuracy} summary={analysis_result.summary} />
+      <ArticleCards accuracy={accuracy} articles={related_articles} />
       <FeedbackCard />
     </>
   );
