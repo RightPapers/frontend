27e0a5b10ba7c24d08feb506897e9fe4132e06d4
b0xercat <seojin3154@naver.com>
2024-08-11 02:59:39 +0900

    refactor: separate deletedialog with history page

--- app/(main)/history/page.tsx
+++ app/(main)/history/page.tsx
@@ -12,65 +12,10 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
 import { IoCloseOutline } from 'react-icons/io5';
+import DeleteDialog from './_components/DeleteDialog';
 
-const DeleteAlertDialog = ({
-  setOpen,
-  setHistories,
-}: {
-  setOpen: (value: boolean) => void;
-  setHistories: (value: YoutubeInfo[]) => void;
-}) => {
-  const { deleteAllResults } = useResultStore();
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger>
-        <div className='cursor-pointer rounded-md p-2 hover:bg-slate-50'>
-          전체 검색 기록 삭제
-        </div>
-      </AlertDialogTrigger>
-      <AlertDialogContent className='w-96 rounded-xl'>
-        <AlertDialogHeader className='text-left'>
-          <AlertDialogTitle>검색 기록을 삭제하시겠습니까?</AlertDialogTitle>
-          <AlertDialogDescription>
-            모든 검색 기록이 삭제되며 복구할 수 없습니다.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter className='flex-row items-center justify-end gap-2'>
-          <AlertDialogCancel
-            className='m-0'
-            onClick={() => {
-              setOpen(false);
-            }}
-          >
-            취소
-          </AlertDialogCancel>
-          <AlertDialogAction
-            onClick={() => {
-              deleteAllResults();
-              setHistories([]);
-              setOpen(false);
-            }}
-            className='bg-red-600 text-white hover:bg-red-600/80'
-          >
-            삭제
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  );
-};
+
 
 const History = () => {
   const results = useResultStore((state) => state.results);
@@ -84,6 +29,11 @@ const History = () => {
     });
   }, [results]);
 
+  const handleDelete = (videoId: string) => {
+    deleteResult(videoId);
+    setHistories((prev) => prev.filter((item) => item.video_id !== videoId));
+  };
+
   return (
     <CardComponent>
       <div className='flex justify-between'>
@@ -96,7 +46,7 @@ const History = () => {
             align={'end'}
             className='-mr-2 w-36 p-0 text-center text-sm'
           >
-            <DeleteAlertDialog
+            <DeleteDialog
               setOpen={setPopoverOpen}
               setHistories={setHistories}
             />
@@ -119,10 +69,7 @@ const History = () => {
                 className='absolute -right-1 top-0 z-10 cursor-pointer text-gray-400'
                 size={20}
                 onClick={() => {
-                  deleteResult(history.video_id);
-                  setHistories((prev) =>
-                    prev.filter((item) => item.video_id !== history.video_id)
-                  );
+                  handleDelete(history.video_id);
                 }}
               />
             </div>
