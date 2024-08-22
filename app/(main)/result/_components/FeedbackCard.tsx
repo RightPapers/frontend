e0093e121fbb827aa0fc32ed70656a7a2f9ac0f9
b0xercat <seojin3154@naver.com>
2024-08-22 16:45:31 +0900

    feat: change feedback card design

--- app/(main)/result/_components/FeedbackCard.tsx
+++ app/(main)/result/_components/FeedbackCard.tsx
@@ -9,6 +9,7 @@ import { useToast } from '@/components/ui/use-toast';
 import { useState } from 'react';
 import TitleComponent from '@/components/TitleComponent';
 import { RiQuestionnaireLine } from 'react-icons/ri';
+import { FaRegThumbsDown, FaRegThumbsUp } from 'react-icons/fa';
 
 const FeedbackCard = ({ video_id }: { video_id: string }) => {
   const { toast } = useToast();
@@ -28,7 +29,6 @@ const FeedbackCard = ({ video_id }: { video_id: string }) => {
         <div className='flex justify-around gap-4'>
           <Button
             variant='icon'
-            className='bg-primary text-primary-foreground hover:bg-primary/90'
             onClick={() => {
               toast({
                 title: '감사합니다!',
@@ -36,13 +36,20 @@ const FeedbackCard = ({ video_id }: { video_id: string }) => {
               });
             }}
           >
-            <HiOutlineThumbUp size={22} />네
+            <FaRegThumbsUp
+              size={22}
+              className='absolute left-5 mobile:left-4'
+            />
+            <p className='absolute right-10 mobile:right-8'>네</p>
           </Button>
           <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
               <Button variant='icon'>
-                <HiOutlineThumbDown size={22} />
-                아니요
+                <FaRegThumbsDown
+                  size={22}
+                  className='absolute left-5 mobile:left-4'
+                />
+                <p className='absolute right-6 mobile:right-4'>아니요</p>
               </Button>
             </DialogTrigger>
             <FeedbackDialog setOpen={setOpen} video_id={video_id} />
